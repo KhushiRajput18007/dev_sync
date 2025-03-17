7595e1d9b9c9ae418b227a9cbf8239216baf7ed7
KhushiRajput182007 <khushi.rajput.cg@gmail.com>
2025-03-17 11:49:39 +0530

    modified some files

---
+++ frontend/dev-sync/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Add your content paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
